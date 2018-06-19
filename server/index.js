937c17b8b1539f9bbac7216758113098cca55520
Sarahi Villalobos <suryvlobos@gmail.com>
2018-06-19 14:21:38 -0700

    Final

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 const port = 9000;
 
@@ -10,6 +13,8 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.listen(port, () => {
   console.log('Successfully listening to port:', port);
 })
+
+
 
 
 
